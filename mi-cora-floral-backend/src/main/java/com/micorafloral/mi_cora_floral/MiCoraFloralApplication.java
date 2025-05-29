1b85e618229f19aba896ae2ab226d7267cc5f777
ricardogodi <gdr1685@gmail.com>
2025-05-29 17:40:01 -0500

    Initial commit: Backend + Frontend setup

---
+++ mi-cora-floral-backend/src/main/java/com/micorafloral/mi_cora_floral/MiCoraFloralApplication.java
@@ -0,0 +1,13 @@
+package com.micorafloral.mi_cora_floral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiCoraFloralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiCoraFloralApplication.class, args);
+	}
+
+}
---
+++ mi-cora-floral-backend/src/test/java/com/micorafloral/mi_cora_floral/MiCoraFloralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micorafloral.mi_cora_floral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiCoraFloralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
